db24c760fed400e499fc5a50c7c9989c3964723a
Royce Joe <roylory@gmail.com>
2025-07-10 05:06:25 -0700

    remove commented code from ExcelCell

--- src/ExcelCell.tsx
+++ src/ExcelCell.tsx
@@ -6,34 +6,9 @@ interface ExcelCellProps {
   value: string | number | boolean | null
 }
 
-// const colorFrom = (argb: string): string => {
-//   return `#${argb.slice(2)}`
-// }
-
 const ExcelCell: React.FC<ExcelCellProps> = ({ rowIndex, value }) => {
-  const style: React.CSSProperties = {}
-
-  // if (
-  //   cell.fill &&
-  //   cell.fill.type === "pattern" &&
-  //   cell.fill?.fgColor
-  // ) {
-  //   const argb = cell.fill.fgColor?.argb
-  //   if (argb) {
-  //     style.backgroundColor = colorFrom(argb) || style.backgroundColor
-  //   }
-  // }
-
-  // if (cell.font?.color) {
-  //   const argb = cell.font.color.argb
-  //   if (argb) {
-  //     style.color = colorFrom(argb) || style.color
-  //   }
-  // }
-
   return (
     <td
-      // style={style}
       className={twMerge(
         "px-2 py-1 text-black dark:text-white border border-gray-300 dark:border-neutral-600",
         rowIndex === 0 && "border-t-0"
